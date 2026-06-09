79b3c0204a1a2925b79fdd3df6a4e50b02fecb73
bora4-netizen <chorkbora4@gmail.com>
2026-06-09 13:20:59 +0700

    everthing done

--- app/Http/Controllers/ColorController.php
+++ app/Http/Controllers/ColorController.php
@@ -10,7 +10,7 @@ class ColorController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request, string $lang)
+    public function index(Request $request)
     {
         //
         $colors = Color::withCount([
