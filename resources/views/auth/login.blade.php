b8b015cff41b8164c4e0e6e2496e47fbd0037c14
bora4-netizen <chorkbora4@gmail.com>
2026-06-10 15:54:42 +0700

    fix login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,22 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
         href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
         rel="stylesheet"
         />
@@ -48,72 +47,73 @@
                                     <div class="card-body">
                                     <!-- Logo -->
                                         <div class="app-brand justify-content-center">
-                                            <img 
-                                                src="{{ $company->image_logo ?? asset('assets/img/logo.png') }}" 
-                                                alt="logo" 
-                                                width="100px"
-                                            />
+                                            <img src="{{ $company->image_logo }}" alt="logo" width="100px"/>
                                         </div>
                                     <!-- /Logo -->
-                                        <h4 class="mb-2 text-center">
-                                            {{ optional($company)->name ?? 'My Phone Shop' }}
-                                        </h4>                                    <p class="mb-4 text-center">Please sign-in to your account.</p>
+                                    <h4 class="mb-2 text-center">{{ $company->name ?? 'CMy Phone ShopE' }}</h4>
+                                    <p class="mb-4 text-center">Please sign-in to your account.</p>
                                     <form id="formAuthentication" class="mb-3" action="{{ route('login') }}" method="POST">
                                         @csrf
                                         <div class="mb-3">
-                                            <!-- <label for="name" class="form-label">Login Name</label> -->
-                                             <label for="email" class="form-label">Email</label>
-                                            <!-- <input id="name" type="name" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="Enter your login name" required autocomplete="name" autofocus> -->
-                                            <input id="email" type="email"
-                                                    class="form-control @error('email') is-invalid @enderror"
-                                                    name="email"
-                                                    value="{{ old('email') }}"
-                                                    placeholder="Enter your email"
-                                                    required autocomplete="email" autofocus>
-                                            <!-- @error('name')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror -->
-                                            @error('email')
+                                            <label for="name" class="form-label">Login Name</label>
+                                            <input id="name" type="name" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="Enter your login name" required autocomplete="name" autofocus>
+
+                                            @error('name')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
                                                 </span>
                                             @enderror
-                                            <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
                                         </div>
-                                    </div>
-                                    <div class="mb-3">
-                                        <button class="btn btn-primary d-grid w-100" type="submit"> {{ __('Login') }}</button>
-                                    </div>
-                                </form>
+                                        <div class="mb-3 form-password-toggle">
+                                            <div class="d-flex justify-content-between">
+                                                <label class="form-label" for="password">Password</label>
+                                                @if (Route::has('password.request'))
+                                                    <a class="btn btn-link d-none" href="{{ route('password.request') }}">
+                                                        <small>{{ __('Forgot Your Password?') }}</small>
+                                                    </a>
+                                                @endif
+                                            </div>
+                                            <div class="input-group input-group-merge">
+                                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password">
+
+                                                @error('password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                                <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                            </div>
+                                        </div>
+                                        <div class="mb-3">
+                                            <button class="btn btn-primary d-grid w-100" type="submit"> {{ __('Login') }}</button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
+                            <!-- /Register -->
                         </div>
-                        <!-- /Register -->
                     </div>
                 </div>
-            </div>
-            <!-- / Content -->
-        </main>
-    </div>
-    <!-- Core JS -->
-    <!-- build:js assets/vendor/js/core.js -->
-    <script src="{{ asset('/assets/vendor/libs/jquery/jquery.js') }} "></script>
-    <script src="{{ asset('/assets/vendor/libs/popper/popper.js') }} "></script>
-    <script src="{{ asset('/assets/vendor/js/bootstrap.js') }} "></script>
-    <script src="{{ asset('/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }} "></script>
+                <!-- / Content -->
+            </main>
+        </div>
+        <!-- Core JS -->
+        <!-- build:js assets/vendor/js/core.js -->
+        <script src="{{ asset('/assets/vendor/libs/jquery/jquery.js') }} "></script>
+        <script src="{{ asset('/assets/vendor/libs/popper/popper.js') }} "></script>
+        <script src="{{ asset('/assets/vendor/js/bootstrap.js') }} "></script>
+        <script src="{{ asset('/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }} "></script>
 
-    <script src="{{ asset('/assets/vendor/js/menu.js') }} "></script>
-    <!-- endbuild -->
+        <script src="{{ asset('/assets/vendor/js/menu.js') }} "></script>
+        <!-- endbuild -->
 
-    <!-- Vendors JS -->
+        <!-- Vendors JS -->
 
-    <!-- Main JS -->
-    <script src="{{ asset('/assets/js/main.js') }} "></script>
+        <!-- Main JS -->
+        <script src="{{ asset('/assets/js/main.js') }} "></script>
 
-    <!-- Page JS -->
-    <!-- Place this tag in your head or just before your close body tag. -->
+        <!-- Page JS -->
+        <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
-</body>
-
+    </body>
 </html>
